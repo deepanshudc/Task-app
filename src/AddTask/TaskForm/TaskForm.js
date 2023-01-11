335fc85c4eadb718c99bbf8f0734000cb520b43a
deepanshu chugh <deepanshuchugh.dc@gmail.com>
2023-01-11 11:39:11 +0530

    handled empty task situation

--- src/AddTask/TaskForm/TaskForm.js
+++ src/AddTask/TaskForm/TaskForm.js
@@ -2,10 +2,20 @@ import { useState } from 'react';
 import './TaskForm.css'
 const TaskForm=(props)=>{
 
+    let active;
+
     const taskHandler=(event)=>{
         setTaskHandler(event.target.value)
     }
     const [enterTask,setTaskHandler]=useState('');
+
+    if(enterTask.length>0){
+       active=false;
+    }
+    else{
+       active=true
+    }
+
 
     const addTask=(event)=>{
         event.preventDefault();
@@ -24,7 +34,7 @@ const TaskForm=(props)=>{
             <div className="task_form">
                 <label>Enter Task:</label>
                 <input type='text' value={enterTask} onChange={taskHandler}></input>
-                <button type="submit" >Create Task</button>
+                <button type="submit" disabled={active} >Create Task</button>
             </div>
         </form>
     )
